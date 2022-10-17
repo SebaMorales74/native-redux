3e68cc77e641505bb93334f1c5cb7a6b0978091f
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2022-10-17 16:56:50 -0300

    init

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 export default function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {data.map((item, index) => {
+        return (
+          <View key={index} style={styles.item}>
+            <Text>{item.name}</Text>
+          </View>
+        )
+      })}
       <StatusBar style="auto" />
     </View>
   );
